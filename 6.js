dd46f814f529aacd516794c146b479713828eb9b
Jorian <jorian@mail.ru>
2021-06-23 14:08:09 +0300

    rabota 23.06 - 5

--- 6.js
+++ 6.js
@@ -1,18 +1,31 @@
 $(function($) {
-    //let modal = '<div class="screen"><div class="modal"><h1>MODAL WINDOW!</h1><div>click me!</div></div></div>';
-    let modal = '';
-    
-    
+    let myobj = {
+        modal: '{"code":200,"success":true,"message":"Операция выполнена успешно!"}',
+        medal: '{"code":300,"success":false,"message":"Операция не выполнена!"}',
+        mindal: '{"code":500,"success":false,"message":"У нас забастовка!"}',
+    }    
+    let arr = ['modal', 'medal', 'mindal']
     
     $('.screener').click(function(){
-        /*
-        $('.container').append(modal);
-        $('.modal > div').on('click', function(){
-            $('.screen').remove();
-        });
-        */
+        let cnt = Math.floor(Math.random() * 3);
+        makeModal(myobj[arr[cnt]]);
     });
     $('.alerter').click(function(){
         alert('OGOGO!!!');
     });
 });
+
+function makeModal(data){
+    str = '<div class="screen"><div class="modal"><h1></h1><p></p><div>click me!</div></div></div>';
+    $('.container').append(str);
+    $('.modal > div').on('click', function(){
+        $('.screen').remove();
+    });
+    let d = JSON.parse(data);
+    if (d.success) {
+        $('h1').html('SUCCESS!');
+    } else {
+        $('h1').html('ERROR!');
+    }
+    $('p').html(d.message);
+}
